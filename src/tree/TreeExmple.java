7b2a985dc644aa7c53dde18c2895d4a7f18207d1
Hari7222 <haripriyajeya@gmail.com>
2023-11-25 12:43:51 +0530

    Tree

--- src/tree/TreeExmple.java
+++ src/tree/TreeExmple.java
@@ -4,28 +4,44 @@ public class TreeExmple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TreeNode<Integer> n=new TreeNode<Integer>(8);
-		 n.Left=new TreeNode<Integer>(6);
-		 n.Right=new TreeNode<Integer>(5);
-		 n.Left.Left=new TreeNode<Integer>(9);
+		TreeNode<Integer> n=new TreeNode<Integer>(19);
+		 n.Left=new TreeNode<Integer>(17);
+		 n.Right=new TreeNode<Integer>(22);
+		 n.Left.Left=new TreeNode<Integer>(7);
 		 n.Left.Right=new TreeNode<Integer>(2);
-		 n.Right.Left=new TreeNode<Integer>(7);
-		 n.Right.Right=new TreeNode<Integer>(1);
-		 n.Left.Left.Left=new TreeNode<Integer>(21);
-		 n.Left.Left.Right=new TreeNode<Integer>(19);
-		 n.Left.Right.Left=new TreeNode<Integer>(3);
-		 n.Left.Right.Right=new TreeNode<Integer>(16);
-		 n.Right.Left.Left=new TreeNode<Integer>(12);
-		 n.Right.Left.Right=new TreeNode<Integer>(13);
-		 
+		 n.Right.Left=new TreeNode<Integer>(8);
+		 n.Right.Right=new TreeNode<Integer>(6);
+		 n.Left.Left.Left=new TreeNode<Integer>(33);
+		 n.Left.Left.Right=new TreeNode<Integer>(31);
+		 n.Left.Right.Left=new TreeNode<Integer>(39);
+		 n.Left.Right.Right=new TreeNode<Integer>(32);
+		 n.Right.Right.Left=new TreeNode<Integer>(16);
+		 n.Right.Right.Right=new TreeNode<Integer>(12);
+		 n.Left.Left.Left.Left=new TreeNode<Integer>(5);
+		 n.Left.Left.Left.Right=new TreeNode<Integer>(29);
+		 n.Left.Left.Right.Left=new TreeNode<Integer>(9);
+		 n.Right.Right.Left.Left=new TreeNode<Integer>(13);
+		 n.Right.Right.Left.Right=new TreeNode<Integer>(14);
+		 n.Right.Right.Left.Right.Right=new TreeNode<Integer>(1);
+		 n.Right.Right.Left.Right.Right.Right=new TreeNode<Integer>(3);
+		/*n.Left=new TreeNode<Integer>(7);
+		n.Right=new TreeNode<Integer>(8);
+		n.Left.Left=new TreeNode<Integer>(1);
+		n.Left.Right=new TreeNode<Integer>(9);
+		n.Left.Right.Right=new TreeNode<Integer>(2);*/
 		 TreeTraversal traversal=new TreeTraversal(n);
+		 System.out.println("Height: "+traversal.height(n));
+		 TreeNode<Integer> mirrorRoot=traversal.mirror(n);
 		 System.out.println("Inorder");
-		 traversal.inOrder();
+		 traversal.inOrder(n);
 		 System.out.println("-----------");
 		 System.out.println("Preorder");
 		 traversal.preOrder();
 		 System.out.println("Postorder");
 		 traversal.postOrder();
+		 System.out.println("Mirror: "+traversal.mirror(n));
+		 
 	}
 
 }
+ 
--- src/tree/TreeTraversal.java
+++ src/tree/TreeTraversal.java
@@ -1,13 +1,46 @@
-  package tree;
+package tree;
 
 public class TreeTraversal {
 	TreeNode<Integer> n;
 	public TreeTraversal(TreeNode<Integer> root) {
-		// TODO Auto-generated constructor stub
 		this.n=root;
 	}
-	public void inOrder() {
+	/*public void inOrder() {
 		inOrder(n);
+	}*/
+	public void pattern(TreeNode<Integer> currentRoot) {
+		
+	}
+	public int height(TreeNode<Integer> currentRoot) {
+		if(currentRoot==null) {
+			return 0;
+		}
+		else {
+			Integer data=currentRoot.data;
+			int leftHeight=height(currentRoot.Left);
+			int rightHeight=height(currentRoot.Right);
+			if(leftHeight>rightHeight) {
+				return leftHeight+1;
+			}
+			else {
+				return rightHeight+1;
+			}
+		}
+		}
+	public TreeNode<Integer> mirror(TreeNode<Integer> currentRoot){
+		if(currentRoot==null) {
+			return currentRoot;
+		}
+		else {
+			TreeNode<Integer> left=mirror(currentRoot.Left);
+			TreeNode<Integer> right=mirror(currentRoot.Right);
+			 
+			currentRoot.Left=right;
+			currentRoot.Right=left;
+			
+			return currentRoot;
+		}
+			
 	}
 	public void inOrder(TreeNode<Integer> currentRoot){
 		if(currentRoot!=null) {
